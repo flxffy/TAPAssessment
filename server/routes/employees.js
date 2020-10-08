a2784bbbf8bc3db53297b4f81e13706fa9170b53
flxffy <aihui@comp.nus.edu.sg>
2020-10-08 21:11:19 +0800

    Creates endpoint for uploading csv

--- server/routes/employees.js
+++ server/routes/employees.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const employee = express.Router();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+const parse = require("csv-parse/lib/sync");
 
 const Employee = require("../models/Employee");
+const employee = express.Router();
 
 employee.get("/", async (req, res) => {
   Employee.find()
@@ -22,6 +26,42 @@ employee.post("/new", async (req, res) => {
   )
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json({ message: err }));
+});
+
+employee.post("/upload/csv", upload.single("file"), async (req, res) => {
+  const b = req.file["buffer"];
+
+  const records = await parse(b.toString(), {
+    columns: ["id", "login", "name", "salary"],
+    comment: "#",
+    trim: true,
+    skip_empty_lines: true,
+  });
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  Promise.all(
+    records.map(async (record) =>
+      Employee.updateOne({ id: { $eq: record.id } }, record, {
+        upsert: true,
+        runValidators: true,
+        session: session,
+      })
+    )
+  )
+    .then(async (data) => {
+      await session.commitTransaction();
+      res.status(200).json(data);
+    })
+    .catch(async (err) => {
+      await session.abortTransaction();
+      res.status(400).json(err);
+    })
+    .finally(() => {
+      session.endSession();
+      res.send();
+    });
 });
 
 module.exports = employee;

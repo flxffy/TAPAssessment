6c8da4d2b057698b6b920d832c6544015f7336d5
flxffy <aihui@comp.nus.edu.sg>
2020-10-09 01:28:08 +0800

    Creates sidebar

---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import useStyles from "./useStyles";
+
+const Sidebar = ({ avatarUrl, username }) => {
+  const styleProps = { drawerWidth: 240 };
+  const classes = useStyles(styleProps);
+
+  const buttons = Array(5).fill("Button");
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      anchor="left"
+      variant="permanent"
+      classes={{ paper: classes.drawerPaper }}
+    >
+      <div className={classes.drawerHeader}>
+        <img src={avatarUrl} alt="User Avatar" className={classes.avatar} />
+        <Typography variant="body1">{username}</Typography>
+      </div>
+      <Divider />
+      <List>
+        {buttons.map((label, index) => (
+          <ListItem button key={`${label}-${index}`}>
+            <ListItemText primary={label} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+Sidebar.propTypes = {
+  avatarUrl: PropTypes.string,
+  username: PropTypes.string,
+};
+
+Sidebar.defaultProps = {
+  avatarUrl:
+    "https://d33wubrfki0l68.cloudfront.net/f61a669729f94718b8cbe980426210986589a92c/e6a9c/images/programmes/products-and-services/askjamie_thumbnail.jpg",
+  username: "Jamie",
+};
+
+export default Sidebar;
---
+++ client/src/components/Sidebar/useStyles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: (props) => props.drawerWidth,
+    borderRadius: "50%",
+  },
+  drawer: {
+    width: (props) => props.drawerWidth,
+  },
+  drawerPaper: {
+    width: (props) => props.drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(1, 0),
+  },
+}));
+
+export default useStyles;
